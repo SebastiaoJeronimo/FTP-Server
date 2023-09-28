30da5360fdc4d4eaaef9077795b306315b917812
Sebastiao <sc.jeronimo@campus.fct.unl.pt>
2023-09-28 11:54:20 +0100

    main argument numeber checker done

--- zFTP-Client.py
+++ zFTP-Client.py
@@ -1,5 +1,3 @@
-#print("Hello Client World!")
-
 from socket import *
 import sys  #needed to access the command-line arguments 
 
@@ -8,5 +6,16 @@ serverPort = 12000                  # socket server port number
 sockBuffer = 2048                   # socket buffer size
 
 def main():
-    clientSocket = socket(AF_INET,SOCK_STREAM)       # create TCP socket
-    clientSocket.connect((serverName, serverPort))   # open TCP connection
+    #clientSocket = socket(AF_INET,SOCK_STREAM)       # create TCP socket
+    #clientSocket.connect((serverName, serverPort))   # open TCP connection
+    len_args = len(sys.argv)       #get number of arguments
+    if (len_args != 3):
+        print ("wrong number of arguments please put only the server name and the UDP Port so the server can recieve commands")
+    else:
+        print("server port" , sys.argv[1]) #after that check if the port is valid
+
+    print("Hello Client World!")
+
+
+if __name__ == "__main__":  #check if the module is being run as the main program
+    main()
--- zFTP-Server.py
+++ zFTP-Server.py
@@ -1,3 +1,20 @@
 import sys  #needed to access the command-line arguments
 
-print("Hello Server World!")
+def main():
+
+    print("Hello Server World!")
+
+    len_args = len(sys.argv)       #get number of arguments
+    if (len_args != 2):
+        print ("wrong number of arguments please put only the UDP Port so the server can recieve commands")
+    else:
+        print("server port" , sys.argv[1]) #after that check if the port is valid
+    print("Number of arguments: ",len_args-1)
+    print("Argument list: ")
+    
+    for i in range(1, len_args):    #list all arguments
+        print(str(sys.argv[i]))
+
+
+if __name__ == "__main__":  #check if the module is being run as the main program
+    main()
