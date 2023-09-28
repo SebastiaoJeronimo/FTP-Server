73bf411820fdbcf441bf79542ea60586c458ad95
Goncalo-MMG-Carvalho <gmm.carvalho@campus.fct.unl.pt>
2023-09-28 10:52:41 +0100

    First iteration

--- zFTP-Client.py
+++ zFTP-Client.py
@@ -1,1 +1,12 @@
-print("Hello Client World!")
+#print("Hello Client World!")
+
+
+from socket import *
+
+serverName = "localhost"            # server name
+serverPort = 12000                  # socket server port number
+sockBuffer = 2048                   # socket buffer size
+
+def main():
+    clientSocket = socket(AF_INET,SOCK_STREAM)       # create TCP socket
+    clientSocket.connect((serverName, serverPort))   # open TCP connection
